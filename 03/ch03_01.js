ea9269012ef2751e2788ebdb898acd476b7a4a63
mhb8436 <mhb8436@gmail.com>
2025-06-18 11:11:45 +0900

    03 추가

---
+++ 03/ch03_01.js
@@ -0,0 +1,5 @@
+// CommonJS 모듈 임포트 방식
+const math = require("./modules/math");
+
+console.log(`math.add(5, 3) => ${math.add(5, 3)}`);
+console.log(`math.subtract(10, 4) => ${math.subtract(10, 4)}`);
