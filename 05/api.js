f8d914235affe5c95cbfc9a9f1aa1b05a4af77d4
mhb8436 <mhb8436@gmail.com>
2025-06-19 16:49:47 +0900

    aa

--- 05/api.js
+++ 05/api.js
@@ -33,6 +33,43 @@ app.post("/posts", (req, res) => {
   db.prepare(sql).run(title, content, author);
   res.status(201).json({ message: "ok" });
 });
+// 게시글 목록 조회
+app.get("/posts", (req, res) => {
+  let sql = `
+        select id, title, content, author, createdAt
+        from posts order by createdAt desc
+    `;
+  const stmt = db.prepare(sql); // 쿼리를 준비하세요
+  const rows = stmt.all(); // 쿼리를 날려주세요
+  console.log(rows);
+  res.status(200).json({ data: rows });
+});
+// 게시글 상세 정보조회
+app.get("/posts/:id", (req, res) => {
+  const id = req.params.id;
+  let sql = `
+        select id, title, content, author, createdAt, count
+        from posts where id = ?
+    `;
+  const stmt = db.prepare(sql); // select 쿼리문이 준비 완료
+  const post = stmt.get(id); //  실제 쿼리문이 실행
+  res.status(200).json({ data: post });
+});
+
+// 게시글 수정
+// http://localhost:3000/posts/1
+app.put("/posts/:id", (req, res) => {
+  const id = req.params.id;
+  const { title, content } = req.body;
+  let sql = `
+        update posts set title = ?, content = ? 
+        where id = ?
+    `;
+  const stmt = db.prepare(sql);
+  stmt.run(title, content, id); // 실제 쿼리문 데이터베이스 실행
+  res.redirect("/posts"); // GET http://localhost:3000/posts
+  //   res.json({ message: "ok" });
+});
 
 // server start
 app.listen(PORT, () => {});
