83a9b1fb6c8b39dfd7c109d8d474ef3e7b8cbeba
mhb8436 <mhb8436@gmail.com>
2025-06-24 15:21:11 +0900

    11

--- 06/ch06_04.js
+++ 06/ch06_04.js
@@ -47,7 +47,56 @@ const User = sequelize.define(
   { tableName: "users" }
 );
 // 2. 게시판모델 정의
+const Post = sequelize.define("Post", {
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  content: {
+    type: DataTypes.TEXT,
+    allowNull: true,
+  },
+  count: { type: DataTypes.INTEGER, defaultValue: 0 },
+});
+
+User.hasMany(Post); // 1(User): N(Post)
+Post.belongsTo(User); // N(Post):1(User)
+// post 테이블에 forgein key 로 user 잡힌다.
 
 (async () => {
   await sequelize.sync({ force: true });
+
+  // Post 테이블에는 1명의 유저 ID 있다
+  // 데이터를 만들어야 합니다.
+
+  // 1. 사용자 정보
+  const user1 = await User.create({
+    username: "홍길동",
+    email: "hong1@email.com",
+    password: "12345678",
+    age: 40,
+  });
+  const user2 = await User.create({
+    username: "트럼프",
+    email: "trump@email.com",
+    password: "12345678",
+    age: 87,
+  });
+  // 2. 게시글 정보
+  const post1 = await Post.create({
+    title: "이란은 언제 공격을 중단할까요?",
+    content: "니네들이 중단하면 우리도 중단한다.",
+    UserId: user2.id,
+  });
+  const post2 = await Post.create({
+    title: "닭도리탕이 먹고싶어요 오늘저녁은...",
+    content: "감자와 수제비도 넣어서 먹고 싶어여 ",
+    UserId: user2.id,
+  });
+  const posts = await Post.findAll({
+    include: {
+      User: true,
+    },
+  });
+  console.log(`posts => ${JSON.stringify(posts)}`);
 })();
