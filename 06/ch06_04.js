e5e351714eef097c598c2f3150958a13adf0a638
mhb8436 <mhb8436@gmail.com>
2025-06-24 14:39:41 +0900

    04

---
+++ 06/ch06_04.js
@@ -0,0 +1,49 @@
+const { Sequelize, Model, DataTypes, Op } = require("sequelize");
+const sequelize = new Sequelize({
+  dialect: "sqlite",
+  storage: "posts.db",
+});
+
+// 1. 회원모델 정의
+const User = sequelize.define(
+  "User",
+  {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [2, 5], // 사용자 이름이 2자리 부터 5자리 까지만 허용
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true, // 이메일 형식이어야 지만 들어올 수 있어용 ~
+      },
+    },
+    password: {
+      // 단방향 암호화 bcrypt
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [6, 20],
+      },
+    },
+    age: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      validate: {
+        min: 0,
+        max: 150,
+      },
+    },
+    status: {
+      type: DataTypes.ENUM("active", "inactive"),
+      defaultValue: "active",
+    },
+  },
+  { tableName: "users" }
+);
+// 2. 게시판모델 정의
