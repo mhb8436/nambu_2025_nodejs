ea53c21971588cc7fc1f00759f927a8bd314f983
mhb8436 <mhb8436@gmail.com>
2025-06-24 12:33:22 +0900

    11

---
+++ 06/ch06_03.js
@@ -0,0 +1,15 @@
+//  CREATE TABLE if not exists todos (
+//         id INTEGER PRIMARY KEY AUTOINCREMENT,
+//         task VARCHAR(255),
+//         description TEXT,
+//         completed BOOLEAN DEFAULT 0,
+//         createdAt datetime default current_timestamp,
+//         priority INTEGER DEFAULT 1
+//     )`;
+
+// 문제 1 : Todo 모델, todos  생성
+// 문제 2 : Todo 데이터를 2개 입력
+// 문제 3 : Todo 데이터를 전체 조회
+// 문제 4 : Todo 아이디가 2번인 항목조회
+// 문제 5 : Todo 아이디가 2번인 항목의 completed 를 완료로 바꿈
+// 문제 6 : Todo 아이디가 2번인 항목 삭제
