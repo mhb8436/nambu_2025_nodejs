1cba8f2a3b69da4dc622e098880b19569e6843b8
mhb8436 <mhb8436@gmail.com>
2025-06-25 11:16:46 +0900

    todos

--- 07/todos.js
+++ 07/todos.js
@@ -31,7 +31,21 @@ app.get("/todos/:id", async (req, res) => {
   }
 });
 
-app.put("/todos/:id", (req, res) => {});
+app.put("/todos/:id", async (req, res) => {
+  const id = req.params.id;
+  const { task, description, completed, priority } = req.body;
+  const todo = await models.Todo.findByPk(id);
+  if (todo) {
+    if (task) todo.task = task;
+    if (description) todo.description = description;
+    if (completed) todo.completed = completed;
+    if (priority) todo.priority = priority;
+    await todo.save();
+    res.status(200).json({ message: "ok", data: todo });
+  } else {
+    res.status(404).json({ message: "할일이 없어" });
+  }
+});
 
 app.delete("/todos/:id", (req, res) => {});
 
