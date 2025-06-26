d588bf268dc70298e05f665d3dc5814bb30dc757
mhb8436 <mhb8436@gmail.com>
2025-06-26 12:34:34 +0900

    router

--- 07/routes/notes.js
+++ 07/routes/notes.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 const noteController = require("../controllers/notes");
 
-router.post("/notes", noteController.createNote);
-router.get("/notes", noteController.getAllNotes);
-router.get("/notes/:tag", noteController.getNotes);
-router.put("/notes/:id", noteController.updateNote);
-router.delete("/notes/:id", noteController.deleteNote);
+// http://localhost:3000/notes/notes
+router.post("/", noteController.createNote);
+router.get("/", noteController.getAllNotes);
+router.get("/:tag", noteController.getNotes);
+router.put("/:id", noteController.updateNote);
+router.delete("/:id", noteController.deleteNote);
 
 module.exports = router;
