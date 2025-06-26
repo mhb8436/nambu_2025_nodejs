91ba8901fc595670f42e9c30385b27b43f3fb390
mhb8436 <mhb8436@gmail.com>
2025-06-26 14:38:09 +0900

    11

---
+++ 07/controllers/posts.js
@@ -0,0 +1,29 @@
+const models = require("../models");
+
+const createPost = async (req, res) => {
+  const { title, content } = req.body;
+  let filename = req.file ? req.file.filename : null; // filename aa-123444.png
+  filename = `/downloads/${filename}`;
+
+  let user = await models.User.findOne({
+    where: { email: "a@example.com" },
+  });
+  if (!user) {
+    user = await models.User.create({
+      name: "이지훈",
+      email: "a@example.com",
+      password: "12345678",
+    });
+  }
+  const post = await models.Post.create({
+    title: title,
+    content: content,
+    authorId: user.id,
+    fileName: filename,
+  });
+  res.status(201).json({ message: "ok", data: post });
+};
+
+module.exports = {
+  createPost,
+};
