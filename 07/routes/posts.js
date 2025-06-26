fa1743070a819a4fc0bec48da8152871feb86f30
mhb8436 <mhb8436@gmail.com>
2025-06-26 16:11:10 +0900

    post 나머지

--- 07/routes/posts.js
+++ 07/routes/posts.js
@@ -4,5 +4,14 @@ const postController = require("../controllers/posts");
 const { uploadSingle, uploadMultiple } = require("../middlewares/upload");
 
 router.post("/", uploadMultiple, postController.createPost);
+router.get("/", postController.findPosts);
+router.get("/:id", postController.findPost);
+router.put("/:id", postController.updatePost);
+router.delete("/:id", postController.deletePost);
+
+router.post("/:postId/comments", postController.createComment);
+router.get("/:postId/comments", postController.findComments);
+router.put("/:postId/comments/:id", postController.updateComment);
+router.delete("/:postId/comments/:id", postController.deleteComment);
 
 module.exports = router;
