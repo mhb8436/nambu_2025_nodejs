d906e52834e9b57e1e7a63a8f6e93d34927ea001
mhb8436 <mhb8436@gmail.com>
2025-06-27 12:17:30 +0900

    login

--- 07/controllers/auth.js
+++ 07/controllers/auth.js
@@ -1,5 +1,6 @@
 const models = require("../models");
 const bcrypt = require("bcryptjs");
+const { generateAccessToken } = require("../utils/token");
 
 const register = async (req, res) => {
   const { email, name, password } = req.body;
@@ -13,6 +14,28 @@ const register = async (req, res) => {
   res.status(201).json({ message: "ok", data: user });
 };
 
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  // 1. 이메일로 사용자가 있는지 확인
+  const user = await models.User.findOne({
+    where: { email: email },
+  });
+  // 2. 사용자가 없으면 잘못된 이메일 비밀번호라고 알려줌
+  if (!user) {
+    return res.status(400).json({ message: "Invalid email and password" });
+  }
+  // 3. 사용자가 있으면 비밀번호 비교
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    // 비밀번호가 일치하지 않으면 사용자에게 노티
+    return res.status(400).json({ message: "Invalid email and password" });
+  }
+  // 4. 정당한 사용자(이메일과 비밀번호가 일치하면) 임시허가증 발급
+  const accessToken = generateAccessToken();
+  res.json({ message: "ok", accessToken: accessToken });
+};
+
 module.exports = {
   register,
+  login,
 };
