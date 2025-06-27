55636d8a9dcd699a5b8430d69c3a3cbf8638e0f4
mhb8436 <mhb8436@gmail.com>
2025-06-27 15:20:35 +0900

    auth.js

--- 07/routes/auth.js
+++ 07/routes/auth.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth");
+const { validateRegister } = require("../middlewares/validation");
 
-router.post("/register", authController.register);
+router.post("/register", validateRegister, authController.register);
 router.post("/login", authController.login);
 module.exports = router;
