54f78a4031a063a8dd2347f0ebc029d09d860b22
mhb8436 <mhb8436@gmail.com>
2025-06-30 12:31:22 +0900

    form model add

--- 07/models/form.js
+++ 07/models/form.js
@@ -45,4 +45,48 @@ module.exports = (sequelize, DataTypes) => {
     },
     { tableName: "answers" }
   );
+
+  Form.associate = function (models) {
+    Form.hasMany(models.Question, {
+      foreignKey: "formId",
+      as: "question",
+    });
+    Form.hasMany(models.Response, {
+      foreignKey: "formId",
+      as: "response",
+    });
+  };
+
+  Question.associate = function (models) {
+    Question.belongsTo(models.Form, {
+      foreignKey: "formId",
+      as: "form",
+    });
+    Question.hasMany(models.Option, {
+      foreignKey: "questionId",
+      as: "option",
+    });
+    Question.hasMany(models.Answer, {
+      foreignKey: "questionId",
+      as: "answer",
+    });
+  };
+
+  Option.associate = function (models) {
+    Option.belongsTo(Question, {
+      foreignKey: "questionId",
+      as: "question",
+    });
+    Option.belongsTo(Answer, {
+      foreignKey: "optionId",
+      as: "answer",
+    });
+  };
+  return {
+    Form,
+    Question,
+    Option,
+    Response,
+    Answer,
+  };
 };
